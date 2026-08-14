8489895c4b1ce5d476d9d77d14ec331e977746e5
gLuca88 <gianluca.88corsi@gmail.com>
2026-08-14 14:30:46 +0200

    feat: aggiunto runner Cucumber con JUnit Platform

---
+++ src/test/java/com/gianluca/runner/RunCucumberTest.java
@@ -0,0 +1,22 @@
+package com.gianluca.runner;
+
+import org.junit.platform.suite.api.*;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = GLUE_PROPERTY_NAME,
+        value = "com.gianluca"
+)
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, summary, io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+)
+public class RunCucumberTest {
+}
+
